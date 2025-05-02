249cda2a43e49c26a50aba4398ab315eebe7997a
MonsieurAmengood <137416510+MonsieurAmengood2@users.noreply.github.com>
2025-05-02 18:08:15 +0100

    Updayte no BackOffice.jsx
    Adicionados botões ao invés de Links no BackOffice.jsx

--- client/src/pages/BackOffice.jsx
+++ client/src/pages/BackOffice.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
-import "../styles/dashboard-backoffice.css"
-import { Link } from 'react-router-dom'; // Importe o Link
+import "../styles/dashboard-backoffice.css";
+import { useNavigate } from 'react-router-dom';
 
 const Backoffice = () => {
-
+  const navigate = useNavigate();
 
   return (
     <div className="area">
       <h1 className="titulo">BackOffice</h1>
       <div className="botoes-container">
-        <Link to="/docentes" className="botao">Docentes</Link>
-        <Link to="/cursos" className="botao">Cursos</Link>
-        <Link to="/unidades-curriculares" className="botao">Unidades Curriculares</Link>
-        <Link to="/escolas" className="botao">Escolas</Link>
-        <Link to="/salas" className="botao">Salas</Link>
+        {/* Botão Docentes */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/docentes')}
+        >
+          Docentes
+        </button>
+
+        {/* Botão Cursos */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/cursos')}
+        >
+          Cursos
+        </button>
+
+        {/* Botão Unidades Curriculares */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/unidades-curriculares')}
+        >
+          Unidades Curriculares
+        </button>
+
+        {/* Botão Escolas */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/escolas')}
+        >
+          Escolas
+        </button>
+
+        {/* Botão Salas */}
+        <button 
+          className="botao" 
+          onClick={() => navigate('/salas')}
+        >
+          Salas
+        </button>
       </div>
     </div>
   );
